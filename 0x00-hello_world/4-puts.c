e2e59abbf8dab90473f81ef8a91bad0a643a1b07
ERIC-NGUGI <ericmwenja@gmil.com>
2022-10-15 03:27:57 +0000

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/*A program that prints programming is like building a multilingual puzzle*/
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
