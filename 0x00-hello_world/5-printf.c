717142733c2348af5923f7a7d1abc6138e6fb52b
ERIC-NGUGI <ericmwenja@gmil.com>
2022-10-15 17:33:36 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*A printf c program*/
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
