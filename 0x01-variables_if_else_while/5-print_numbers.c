bd540a8caa1de0a5921be2f836ed1b83a56fa2ff
ERIC-NGUGI <ericmwenja@gmil.com>
2022-10-18 10:17:43 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print lowercase alphabets, except q and e.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		printf("%d\n", number);
+	}
+
+	return (0);
+}
+
