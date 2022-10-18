f621a406005de291a653f7428ca9382ee2fbae98
ERIC-NGUGI <ericmwenja@gmil.com>
2022-10-18 13:42:04 +0000

    business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 	{
 		for (n2 = n1 + 1; n2 < 9; n2++)
 		{
-			for (n3 = n2 + 1; n3 < 8; n3++)
+			for (n3 = n2 + 1; n3 < 10; n3++)
 			{
 				putchar((n1 % 10) + '0');
 				putchar((n2 % 10) + '0');
