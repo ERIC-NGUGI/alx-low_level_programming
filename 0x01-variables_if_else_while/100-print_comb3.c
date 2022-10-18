fb9df891b3900a3399c239defc5b70d41791432b
ERIC-NGUGI <ericmwenja@gmil.com>
2022-10-18 12:42:01 +0000

    brains

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  * main - entry block
+  *
+  * Return: 0
+  **/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 90; n++)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+
+	return (0);
+}
