48fcb14d7409389ec2de3d352dbeb1a5528b4f25
ERIC-NGUGI <ericmwenja@gmil.com>
2022-10-26 21:26:16 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,16 +7,16 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, index;
 
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = size - j; j > 0; j--)
+			for (index = size - i; index > 0; index--)
 				_putchar(' ');
 
-			for (j = 0; j < i; j++)
+			for (index = 0; index < i; index++)
 				_putchar('#');
 
 			if (i == size)
