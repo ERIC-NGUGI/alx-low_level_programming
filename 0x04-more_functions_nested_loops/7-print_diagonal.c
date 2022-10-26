51ced744e68969709a5863aa63c7876aa7d424f7
ERIC-NGUGI <ericmwenja@gmil.com>
2022-10-26 19:15:26 +0000

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_diagonal - Draws a diagonal line using the \ character.
+* @n: The number of \ characters to be printed.
+*/
+
+void print_diagonal(int n)
+{
+	int diag, space;
+
+	if (n > 0)
+	{
+		for (diag = 0; diag < n; diag++)
+		{
+			for (space = 0; space < diag; space++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (diag == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
