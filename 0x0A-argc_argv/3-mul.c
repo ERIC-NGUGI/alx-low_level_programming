d2386e3e8b758876b8d17d8dd181923dfd4cf915
ERIC-NGUGI <ericmwenja@gmil.com>
2022-11-15 08:33:19 +0000

    multiple

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, multiple = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			multiple = multiple * atoi(argv[i]);
+		}
+		printf("%d\n", multiple);
+	}
+	return (0);
+}
