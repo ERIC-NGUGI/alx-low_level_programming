2a3948bc997d6e98560df8b204b370adf33b9007
ERIC-NGUGI <ericmwenja@gmil.com>
2022-11-21 02:11:41 +0000

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+* struct dog - dog data
+* @name: name of dog
+* @age: age of dog
+* @owner: name of owner dogs
+* Description: Defines metadata about the dog and type of dogs
+*/
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+
+/* function prototypes*:/
 
 #endif
