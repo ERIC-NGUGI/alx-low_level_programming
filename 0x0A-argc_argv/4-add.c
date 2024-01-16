c4cde30b38b54b088409130940764d79dbd36e7b
ERIC-NGUGI <ericmwenja@gmail.com>
2024-01-16 14:30:45 +0000

    addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int num, i, addition = 1;
+
+	if (argc < 3)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		char *endptr;
+		num = strtol(argv[i], &endptr, 10);
+
+		if (*endptr != '\0' && *endptr != '\n')
+		{
+			printf("Error\n");
+			return (1);
+		}
+	addition += num;
+	}
+	
+
+	printf("%d\n", addition);
+
+	return (0);
+}
