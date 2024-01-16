21748fa618adffd14ea49318fdf05ceeb0481029
ERIC-NGUGI <ericmwenja@gmail.com>
2024-01-16 14:47:28 +0000

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	int num, i, addition = 0;
